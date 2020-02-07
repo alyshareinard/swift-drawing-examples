b3673cef32de15cfb94dbb12f34f474281014594
alysha.reinard@gmail.com <alysha.reinard@noaa.gov>
2020-02-07 10:45:04 +0100

    Initial Commit

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Alysha Reinard on 2/7/20.
+//  Copyright © 2020 Alysha Reinard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
